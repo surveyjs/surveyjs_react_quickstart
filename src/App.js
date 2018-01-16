476f1047076ffa814166a62ee0fa7c1ab6dc7b49
tsv2013 <tsv2013@users.noreply.github.com>
2018-01-16 09:16:37 +0300

    Added custom widgets (signature pad) to the project, updated dependency versions

--- src/App.js
+++ src/App.js
@@ -1,116 +1,146 @@
-import React, { Component } from 'react';
-import * as Survey from 'survey-react';
-import 'survey-react/survey.css';
-import SurveyEditor from './SurveyEditor';
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import React, { Component } from "react";
+import * as Survey from "survey-react";
+import "survey-react/survey.css";
+import SurveyEditor from "./SurveyEditor";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import * as widgets from "surveyjs-widgets";
+
+widgets.signaturepad(Survey);
 
 class App extends Component {
-  json = { title: 'Product Feedback Survey Example', showProgressBar: 'top', pages: [
+  json = {
+    title: "Product Feedback Survey Example",
+    showProgressBar: "top",
+    pages: [
       {
-        questions: [{
-            type: 'matrix',
-            name: 'Quality',
-            title: 'Please indicate if you agree or disagree with the following statements',
-            columns: [{
+        questions: [
+          {
+            type: "signaturepad",
+            width: "500px",
+            name: "Signature Pad - you can enter your signature here:"
+          },
+          {
+            type: "matrix",
+            name: "Quality",
+            title:
+              "Please indicate if you agree or disagree with the following statements",
+            columns: [
+              {
                 value: 1,
-                text: 'Strongly Disagree'
+                text: "Strongly Disagree"
               },
               {
                 value: 2,
-                text: 'Disagree'
+                text: "Disagree"
               },
               {
                 value: 3,
-                text: 'Neutral'
+                text: "Neutral"
               },
               {
                 value: 4,
-                text: 'Agree'
+                text: "Agree"
               },
               {
                 value: 5,
-                text: 'Strongly Agree'
+                text: "Strongly Agree"
               }
             ],
-            rows: [{
-                value: 'affordable',
-                text: 'Product is affordable'
+            rows: [
+              {
+                value: "affordable",
+                text: "Product is affordable"
               },
               {
-                value: 'does what it claims',
-                text: 'Product does what it claims'
+                value: "does what it claims",
+                text: "Product does what it claims"
               },
               {
-                value: 'better then others',
-                text: 'Product is better than other products on the market'
+                value: "better then others",
+                text: "Product is better than other products on the market"
               },
               {
-                value: 'easy to use',
-                text: 'Product is easy to use'
+                value: "easy to use",
+                text: "Product is easy to use"
               }
             ]
           },
           {
-            type: 'rating',
-            name: 'satisfaction',
-            title: 'How satisfied are you with the Product?',
-            mininumRateDescription: 'Not Satisfied',
-            maximumRateDescription: 'Completely satisfied'
+            type: "rating",
+            name: "satisfaction",
+            title: "How satisfied are you with the Product?",
+            mininumRateDescription: "Not Satisfied",
+            maximumRateDescription: "Completely satisfied"
           },
           {
-            type: 'rating',
-            name: 'recommend friends',
-            visibleIf: '{satisfaction} > 3',
-            title: 'How likely are you to recommend the Product to a friend or co-worker?',
-            mininumRateDescription: 'Will not recommend',
-            maximumRateDescription: 'I will recommend'
+            type: "rating",
+            name: "recommend friends",
+            visibleIf: "{satisfaction} > 3",
+            title:
+              "How likely are you to recommend the Product to a friend or co-worker?",
+            mininumRateDescription: "Will not recommend",
+            maximumRateDescription: "I will recommend"
           },
           {
-            type: 'comment',
-            name: 'suggestions',
-            title: 'What would make you more satisfied with the Product?',
+            type: "comment",
+            name: "suggestions",
+            title: "What would make you more satisfied with the Product?"
           }
         ]
-      }, {
-        questions: [{
-            type: 'radiogroup',
-            name: 'price to competitors',
-            title: 'Compared to our competitors, do you feel the Product is',
-            choices: ['Less expensive', 'Priced about the same', 'More expensive', 'Not sure']
-          },
+      },
+      {
+        questions: [
           {
-            type: 'radiogroup',
-            name: 'price',
-            title: 'Do you feel our current price is merited by our product?',
-            choices: ['correct|Yes, the price is about right',
-              'low|No, the price is too low for your product',
-              'high|No, the price is too high for your product'
+            type: "radiogroup",
+            name: "price to competitors",
+            title: "Compared to our competitors, do you feel the Product is",
+            choices: [
+              "Less expensive",
+              "Priced about the same",
+              "More expensive",
+              "Not sure"
             ]
           },
           {
-            type: 'multipletext',
-            name: 'pricelimit',
-            title: 'What is the... ',
-            items: [{
-                name: 'mostamount',
-                title: 'Most amount you would every pay for a product like ours'
+            type: "radiogroup",
+            name: "price",
+            title: "Do you feel our current price is merited by our product?",
+            choices: [
+              "correct|Yes, the price is about right",
+              "low|No, the price is too low for your product",
+              "high|No, the price is too high for your product"
+            ]
+          },
+          {
+            type: "multipletext",
+            name: "pricelimit",
+            title: "What is the... ",
+            items: [
+              {
+                name: "mostamount",
+                title: "Most amount you would every pay for a product like ours"
               },
               {
-                name: 'leastamount',
-                title: 'The least amount you would feel comfortable paying'
+                name: "leastamount",
+                title: "The least amount you would feel comfortable paying"
               }
             ]
           }
         ]
-      }, {
-        questions: [{
-          type: 'text',
-          name: 'email',
-          title: 'Thank you for taking our survey. Please enter your email address, then press the "Submit" button.'
-        }]
-      }]
+      },
+      {
+        questions: [
+          {
+            type: "text",
+            name: "email",
+            title:
+              'Thank you for taking our survey. Please enter your email address, then press the "Submit" button.'
+          }
+        ]
+      }
+    ]
   };
 
   render() {
@@ -124,9 +154,11 @@ class App extends Component {
         </div>
         <div className="surveyjs">
           {/*If you want to show survey, uncomment the line below*/}
-          <Survey.Survey model={model}/>
+          <h1>SurveyJS library in action:</h1>
+          <Survey.Survey model={model} />
           {/*If you want to show survey editor, uncomment the line below*/}
-          {/*<SurveyEditor />*/}
+          <h1>SurveyJS Editor in action:</h1>
+          <SurveyEditor />
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
--- src/SurveyEditor.js
+++ src/SurveyEditor.js
@@ -1,22 +1,27 @@
-import React, { Component } from 'react';
-import * as SurveyJSEditor from 'surveyjs-editor';
-import 'surveyjs-editor/surveyeditor.css';
+import React, { Component } from "react";
+import * as SurveyJSEditor from "surveyjs-editor";
+import * as SurveyKo from "survey-knockout";
+import "surveyjs-editor/surveyeditor.css";
+import * as widgets from "surveyjs-widgets";
+
+widgets.signaturepad(SurveyKo);
 
 class SurveyEditor extends Component {
   editor;
   componentDidMount() {
     let editorOptions = { showEmbededSurveyTab: true };
-    this.editor = new SurveyJSEditor.SurveyEditor('surveyEditorContainer', editorOptions);
+    this.editor = new SurveyJSEditor.SurveyEditor(
+      "surveyEditorContainer",
+      editorOptions
+    );
     this.editor.saveSurveyFunc = this.saveMySurvey;
   }
   render() {
-    return (
-      <div id="surveyEditorContainer"></div>
-    );
+    return <div id="surveyEditorContainer" />;
   }
   saveMySurvey = () => {
-      console.log(JSON.stringify(this.editor.text));
-  }
+    console.log(JSON.stringify(this.editor.text));
+  };
 }
 
 export default SurveyEditor;
