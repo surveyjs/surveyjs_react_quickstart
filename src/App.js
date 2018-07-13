bdd6a925b37e7b830c2ec54b4e9873272da2ef69
tsv2013 <tsv2013@users.noreply.github.com>
2018-07-13 09:26:00 +0300

    Got rid of image picker CW

--- src/App.js
+++ src/App.js
@@ -10,21 +10,18 @@ import "jquery-ui/themes/base/all.css";
 import "nouislider/distribute/nouislider.css";
 import "select2/dist/css/select2.css";
 import "bootstrap-slider/dist/css/bootstrap-slider.css";
-import "image-picker/image-picker/image-picker.css";
 
 import "jquery-bar-rating/dist/themes/css-stars.css";
 
 import $ from "jquery";
 import "jquery-ui/ui/widgets/datepicker.js";
 import "select2/dist/js/select2.js";
-import "image-picker/image-picker/image-picker.js";
 import "jquery-bar-rating";
 
 import * as widgets from "surveyjs-widgets";
 
 widgets.icheck(Survey, $);
 widgets.select2(Survey, $);
-widgets.imagepicker(Survey, $);
 widgets.inputmask(Survey);
 widgets.jquerybarrating(Survey, $);
 widgets.jqueryuidatepicker(Survey, $);
