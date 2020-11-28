9c337cf697dd7a0869874117ca642b8df184dbe1
sloth <dk981234@gmail.com>
2020-11-28 21:31:58 +0300

    Add jspdf and xlsx dependencies for tabulator

--- src/SurveyAnalyticsTabulator.js
+++ src/SurveyAnalyticsTabulator.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
 import { data, json } from "./analytics_data";
+
+import * as jsPDF from "jspdf";
+import * as XLSX from "xlsx";
+import "jspdf-autotable";
+
+
+
 import { Tabulator } from "survey-analytics/survey.analytics.tabulator.js";
 import * as Survey from "survey-react";
 import "survey-analytics/survey.analytics.tabulator.css";
 import "tabulator-tables/dist/css/tabulator.min.css";
+
+
+window.jsPDF = jsPDF;
+window.XLSX = XLSX;
+
 export default class SurveyAnalyticsTabulator extends Component {
   visPanel;
   componentDidMount() {
