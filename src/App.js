2c156f2e823960a4b9862ce4da95fb955e327f21
Andrew <andrew.telnov@gmail.com>
2025-01-30 17:46:28 +0200

    Update App.js
    Remove custom widgets

--- src/App.js
+++ src/App.js
@@ -10,42 +10,7 @@ import { AnalyticsDatatablesPage } from "./pages/AnalyticsDatatables";
 
 import "bootstrap/dist/css/bootstrap.css";
 
-import * as Survey from "survey-core";
-import $ from "jquery";
-
 export { MyQuestion } from "./components/MyQuestion";
-
-import "jquery-ui/themes/base/all.css";
-import "nouislider/distribute/nouislider.css";
-import "select2/dist/css/select2.css";
-import "bootstrap-slider/dist/css/bootstrap-slider.css";
-import "jquery-bar-rating/dist/themes/css-stars.css";
-import "jquery-bar-rating/dist/themes/fontawesome-stars.css";
-import "pretty-checkbox/dist/pretty-checkbox.css";
-
-import "select2/dist/js/select2.js";
-import "jquery-bar-rating";
-
-// import "icheck/skins/square/blue.css";
-// require("icheck");
-
-import * as widgets from "surveyjs-widgets";
-
-window["$"] = window["jQuery"] = $;
-require("jquery-ui/ui/widgets/datepicker.js");
-
-widgets.prettycheckbox(Survey);
-widgets.select2(Survey, $);
-widgets.inputmask(Survey);
-widgets.jquerybarrating(Survey, $);
-widgets.jqueryuidatepicker(Survey, $);
-widgets.nouislider(Survey);
-widgets.select2tagbox(Survey, $);
-widgets.sortablejs(Survey);
-widgets.ckeditor(Survey);
-widgets.autocomplete(Survey, $);
-widgets.bootstrapslider(Survey);
-// widgets.icheck(Survey, $);
 
 export default function SurveyJSReactApplication() {
   return (
